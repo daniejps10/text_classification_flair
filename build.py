e3226d2084e6533ec5c6e971e51b5a241cbf376d
Daniel Javier Pulla S <daniel.javier.pulla@yandex.com>
2023-01-09 10:29:12 -0500

    test(Classifier): rebuilding the dataset

---
+++ build.py
@@ -0,0 +1,42 @@
+# Based on: How to Create an NLP Application using Flair
+# Link: https://www.section.io/engineering-education/how-to-create-nlp-application-with-flair/
+
+#Using a Pre-trained Classification Model
+import constants
+import numpy as np
+import pandas as pd
+
+#Read data
+print(constants.LINE)
+print(constants.SUB_LINE)
+print('Read SPAM file...')
+data_df = pd.read_csv(constants.INPUT_FILE).sample(frac=1).drop_duplicates()
+print(data_df.head())
+
+# Checking value counts
+print(constants.SUB_LINE)
+print('Checking value counts...')
+print(data_df['class'].value_counts())
+print('Show columns...')
+print(data_df.columns)
+
+# Select two columns: clean_tweet, labels
+print(constants.SUB_LINE)
+print('Selecting two columns...')
+data_df = data_df[['clean_tweet','labels']]
+
+#Rename columns
+print(constants.SUB_LINE)
+print('Rename columns...')
+data_df = data_df.rename(columns={"clean_tweet":"text"})
+#data['label'] = '__label__' + data['label'].astype(str)
+print(data_df.head())
+
+#Split into train, test, dev data
+print(constants.SUB_LINE)
+print('Split into train, test, dev data...')
+train_df,test_df, dev_df = np.split(data_df,[int(.6*len(data_df)),int(.8*len(data_df))])
+print(data_df.shape)
+print(train_df.shape)
+print(test_df.shape)
+print(dev_df.shape)
--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 #Constants
 LINE = '-'*60
 SUB_LINE = '-'*30
+
+INPUT_FILE = 'offensive_and_non_offensive_dataset.csv'
